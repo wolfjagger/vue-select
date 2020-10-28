336e532aef5ba6a1d6c7bb1a8206f1e6d0e5dc20
joe <mitchelljoe3@gmail.com>
2020-10-27 22:30:16 -0400

    Use get('input') in place of find({ ref: 'search' }) in helpers

--- tests/helpers.js
+++ tests/helpers.js
@@ -13,7 +13,7 @@ export const searchSubmit = (Wrapper, searchText = false) => {
   if (searchText) {
     Wrapper.vm.search = searchText;
   }
-  Wrapper.find({ ref: "search" }).trigger("keydown.enter")
+  Wrapper.get("input").trigger("keydown.enter")
 };
 
 /**
@@ -29,8 +29,7 @@ export const selectTag = async (Wrapper, searchText) => {
   Wrapper.vm.search = searchText;
   await Wrapper.vm.$nextTick();
 
-  Wrapper.find({ ref: "search" }).trigger("keydown.enter");
-  await Wrapper.vm.$nextTick();
+  await Wrapper.get("input").trigger("keydown.enter");
 };
 
 /**
